f3e0becf92e1198b689498d8d7c8ca86827de0bb
Michael Kammer <michael.kammer@mailbox.org>
2019-11-18 11:10:15 +0100

    DEV: Updated package documentation and post_processing
    - renamed truncate to process_truncate to not shadow the base::truncate function
    - reworked post-processing a little
    - fixed documentation

--- R/post_processing.R
+++ R/post_processing.R
@@ -4,7 +4,7 @@
 #' @description
 #' Applies functions to a matrix or data.frame.
 #'
-#' @param x
+#' @param X
 #' Matrix or Data.frame.
 #' @param functions
 #' List of lists, specifying functions to be applied as well as their
@@ -12,7 +12,7 @@
 #'
 #' @details
 #' Functions are passed into the post-processor as a named list. The name
-#' \code{f} of the list entry is the function to be applied via do.call.
+#' \code{f} of the list entry is the function to be applied via \code{\link{do.call}}.
 #' The list entry itself is another named list, specifying the arguments
 #' to the function \code{f} as named arguments.
 #'
@@ -20,10 +20,11 @@
 #' return another matrix or data.frame of the same dimensions as
 #' single output.
 #'
-#' Examples of post-processing steps are truncation (\code{\link{truncate}})
-#' or centering / standardizing data.
+#' Examples of post-processing steps are truncation (\code{\link{process_truncate}})
+#' or centering / standardizing data (via \code{\link{scale}}, see example section
+#' below).
 #'
-#' Can be useful to apply on simulated datasets outside of the
+#' Can be useful to apply on simulated datasets, even outside of the
 #' simulation function (e.g. when standardization is only required at the
 #' modeling step).
 #'
@@ -34,73 +35,75 @@
 #'
 #' @examples
 #' \dontrun{
-#' process_data(diag(5), functions = list(scale = list(center = TRUE,
-#' scale = FALSE)))
+#' process_data(diag(5), 
+#'   functions = list(scale = list(center = TRUE, scale = FALSE)))
 #' }
+#' 
+#' @seealso 
+#' \code{\link{process_truncate}}
 #'
 #' @export
-process_data <- function(x, functions = list()) {
+process_data <- function(X, functions = list()) {
 
     # apply post-processing functions
     for (f in names(functions)) {
-        x = do.call(f, modifyList(list(x = x), functions[[f]]))
+        X = do.call(f, modifyList(list(X), functions[[f]]))
     }
 
-    return(x)
+    X
 }
 
 # Post-processing Functions #########################################
 #' @title Truncate columns of datamatrix
 #'
-#' @param x
+#' @param X
 #' Matrix or Data.frame.
 #' @param truncate_multipliers
 #' Vector of truncation parameters. Either a single value which is
-#' replicated as necessaary or of same dimension as \code{ncol(x)}.
+#' replicated as necessaary or of same dimension as \code{ncol(X)}.
 #' If any vector entry is NA, the corresponding column will not be
 #' truncated. See details.
 #'
 #' @details
 #' Truncation is processed as follows:
 #' \enumerate{
-#' \item Compute the 1st and 3rd quartile q1 / q3 of transformed variables
-#' in X.
+#' \item Compute the 1st and 3rd quartile q1 / q3 of variables in _X_.
 #' \item Multiply these quantities by values in \code{truncate_final} to obtain
-#' L1 and U1. If a value is NA, the corresponding variable is not truncated.
-#' \item Set any value smaller / larger than L1 / U1 to L1 / U1.
+#' L and U. If a value is NA, the corresponding variable is not truncated.
+#' \item Set any value smaller / larger than L / U to L / U.
 #' }
 #'
 #' @return
 #' Matrix or Data.frame of same dimensions as input.
 #'
 #' @export
-truncate <- function(x, truncate_multipliers = NA) {
+process_truncate <- function(X, truncate_multipliers = NA) {
 
-    # if single input -> replicate to fit x
-    if (!length(truncate_multipliers) == ncol(x))
-        truncate_multipliers = rep(truncate_multipliers, ncol(x))
+    # if single input -> replicate to fit X
+    truncate_multipliers = rep_len(truncate_multipliers, ncol(X))
 
     # which columns should be truncated
     do_trunc = which(!sapply(truncate_multipliers, is.na))
 
     if (length(do_trunc) > 0) {
         # determine thresholds to truncate at
-        quantiles = apply(x[, do_trunc, drop = FALSE], 2,
+        quantiles = apply(X[, do_trunc, drop = FALSE], 2,
                           quantile, c(0.25, 0.75))
+        iqr = apply(quantiles, 2, diff)
         truncate_upper = quantiles[2, ] +
-            truncate_multipliers[do_trunc] * apply(quantiles, 2, diff)
+            truncate_multipliers[do_trunc] * iqr
         truncate_lower = quantiles[1, ] -
-            truncate_multipliers[do_trunc] * apply(quantiles, 2, diff)
+            truncate_multipliers[do_trunc] * iqr
 
         # do truncation
         for (i in seq_along(do_trunc)) {
-            x[x[, do_trunc[i]] > truncate_upper[i], do_trunc[i]] =
+            X[X[, do_trunc[i]] > truncate_upper[i], do_trunc[i]] =
                 truncate_upper[i]
-            x[x[, do_trunc[i]] < truncate_lower[i], do_trunc[i]] =
+            X[X[, do_trunc[i]] < truncate_lower[i], do_trunc[i]] =
                 truncate_lower[i]
         }
     }
 
-    return(x)
+    X
 }
 
